720fdae497d4ad6bcfed2c24320a12f3d08b3dc3
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-15 20:29:40 +0600

    Node.js Stream & Buffe

--- stream-buffer/index.js
+++ stream-buffer/index.js
@@ -1,6 +1,11 @@
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
-const myReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, "utf8");
-myReadStream.on("data", (data) => {
-  console.log(data);
+const server = http.createServer((req, res) => {
+    const myReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
+    myReadStream.pipe(res);
 });
+
+server.listen(3000);
+
+console.log('listening on port 3000');
