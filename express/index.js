56912b06ad6faba0f746711db07daffb54bb98b8
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-16 18:36:33 +0600

    express.raw() function

--- express/index.js
+++ express/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+app.use(express.raw());
 
 app.get("/", (req, res) => {
   res.send("this is home page");
 });
 app.post("/", (req, res) => {
   console.log(req.body);
-  console.log(req.body.name);
-  console.log(typeof req.body);
   res.send("this is home page with post request");
 });
 
